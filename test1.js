fdc1403bc5e6390376b35ea3cb48c7c13a18c17b
onumam21 <165861782+onumam21@users.noreply.github.com>
2024-07-15 20:26:30 +0700

    Update test1.js

--- test1.js
+++ test1.js
@@ -10,4 +10,7 @@ function sumOfTwoInArray(arr, target) {
   return false;
 }
 
-console.log(numSet);
+console.log(sumOfTwoInArray([1, 3, 5, 7, 16, 4], 8));
+console.log(sumOfTwoInArray([1, 3, 5, 7, 16, 4], 12));
+console.log(sumOfTwoInArray([1, 3, 5, 7, 16, 4], 18));
+console.log(sumOfTwoInArray([12, 17, 14, 11, 19, 8], 20));
